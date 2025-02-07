19ee069e1da3c824d03d179fcea73fb079661e1c
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-02-07 14:26:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // BOT TELE
 module.exports = {
-  BOT_TOKEN: "7688717854:AAEV53wtuq30LKHLo5iwUaKsXxEIDFG3nYE", // Token bot Telegram
+  BOT_TOKEN: "7549474841:AAG78UjaCRBwE63TNlNst_ZEc-hjJcwREjU", // Token bot Telegram
   OWNER_ID: "5965707646", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
